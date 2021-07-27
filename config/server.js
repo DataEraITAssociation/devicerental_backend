a55d8592bf68d84fa6f2c80e4110758dcf2374ad
IvanTheWonderful <ivanhfwang@gmail.com>
2021-07-27 15:01:17 -0600

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5368086c5b6255dddbde8c03621dfba5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '76db1fcd-e795-4d90-874a-d173151fbcb6'
+};
